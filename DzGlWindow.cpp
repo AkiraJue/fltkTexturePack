209f4ccba703f2128ebf771bd9e30a078843476f
AkiraJue <dzsoftware@msn.cn>
2015-02-04 22:47:53 +0800

    Update DzGlWindow.cpp

--- DzGlWindow.cpp
+++ DzGlWindow.cpp
@@ -23,6 +23,39 @@ void CDzGlWindow::draw() {
 		glLoadIdentity();
 		glViewport(0, 0, w(), h());
 	}
+
+	printf("screen W:%d H:%d\n", w(), h());
+
+	//显示纹理
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	
+	glViewport(0, 0, w(), h());
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	glEnable(GL_TEXTURE_2D);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	glBindTexture(GL_TEXTURE_2D, texName);
+	
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 1.0f); glVertex2f(0.0f, 0.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex2f(1.0f, 0.0f);
+	glTexCoord2f(1.0f, 0.0f); glVertex2f(1.0f, 1.0f);
+	glTexCoord2f(0.0f, 0.0f); glVertex2f(0.0f, 1.0f);
+	glEnd();
+
+	glFlush();
+}
+
+void CDzGlWindow::draw_overlay() 
+{
+	// the valid() property may be used to avoid reinitializing your
+	// GL transformation for each redraw:
+}
+
+void CDzGlWindow::LoadTexture(void)
+{
 	//创建材质
 	Fl_PNG_Image img("D:\\1.png");
 
@@ -43,20 +76,12 @@ void CDzGlWindow::draw() {
 
 	case 3://RGB
 		pPng = (colorRGB*)new uchar[img.w() * img.h() * img.d()];
-		for (int y = 0; y < img.h(); y++)
-		{
-			for (int x = 0; x < img.w(); x++)
-			{
-				pPng[y*img.w() + x].r = img.array[y * img.w() + x * img.d() + 2];
-				pPng[y*img.w() + x].g = img.array[y * img.w() + x * img.d() + 1];
-				pPng[y*img.w() + x].b = img.array[y * img.w() + x * img.d() + 0];
-			}
-		}
+		memset(pPng, 0, img.w() * img.h() * img.d());
+		memcpy(pPng, img.array, img.w() * img.h() * img.d());
 
 		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 		glGenTextures(1, &texName);
 		glBindTexture(GL_TEXTURE_2D, texName);
-
 
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -67,22 +92,12 @@ void CDzGlWindow::draw() {
 
 	case 4://RGBA
 		pPngA = (colorRGBA*)new uchar[img.w() * img.h() * img.d()];
-		for (int y = 0; y < img.h(); y++)
-		{
-			for (int x = 0; x < img.w(); x++)
-			{
-				int n = 0;
-				pPngA[y*img.w() + x].r = img.array[y*img.w() + x * 4 + 3];
-				pPngA[y*img.w() + x].g = img.array[y*img.w() + x * 4 + 2];
-				pPngA[y*img.w() + x].b = img.array[y*img.w() + x * 4 + 1];
-				pPngA[y*img.w() + x].a = img.array[y*img.w() + x * 4 + 0];
-			}
-		}
+		memset(pPngA, 0, img.w() * img.h() * img.d());
+		memcpy(pPngA, img.array, img.w() * img.h() * img.d());
 
 		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 		glGenTextures(1, &texName);
 		glBindTexture(GL_TEXTURE_2D, texName);
-
 
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -101,40 +116,6 @@ void CDzGlWindow::draw() {
 
 	if (pPngA != nullptr)
 		delete[]pPngA;
-
-
-	//显示纹理
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	
-	glViewport(0, 0, img.w(), img.h());
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0);
-	glClear(GL_COLOR_BUFFER_BIT);
-	
-	glEnable(GL_TEXTURE_2D);
-	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-	glBindTexture(GL_TEXTURE_2D, texName);
-	
-	glBegin(GL_QUADS);
-	glTexCoord2f(0.0f, 1.0f); glVertex2f(0.0f, 0.0f);
-	glTexCoord2f(1.0f, 1.0f); glVertex2f(1.0f, 0.0f);
-	glTexCoord2f(1.0f, 0.0f); glVertex2f(1.0f, 1.0f);
-	glTexCoord2f(0.0f, 0.0f); glVertex2f(0.0f, 1.0f);
-	glEnd();
-
-	glFlush();
-
-	//img.draw(0, 0);
+		
 	img.uncache();
-}
-
-void CDzGlWindow::draw_overlay() 
-{
-	// the valid() property may be used to avoid reinitializing your
-	// GL transformation for each redraw:
-}
-
-void CDzGlWindow::LoadTexture(void)
-{
-
 }
