dc002699d5f3a489d8f7f69bb29a24be7cf242ff
AkiraJue <dzsoftware@msn.cn>
2015-02-05 15:32:22 +0800

    Update DzGlWindow.h

--- DzGlWindow.h
+++ DzGlWindow.h
@@ -3,6 +3,10 @@
 #include <Fl/gl.h>
 #include <Fl/Fl_Gl_Window.H>
 #include <Fl/Fl_PNG_Image.H>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 class CDzGlWindow : public Fl_Gl_Window 
 {
@@ -24,16 +28,40 @@ public://enum
 		unsigned char b;
 	};
 
+	enum colorType
+	{
+		eRGB,
+		eRGBA,
+		eColorMax
+	};
+
+	struct STexInfo
+	{
+		GLuint tex;		//贴图句柄
+		colorType type;	//贴图类型
+		int width;		//贴图宽
+		int height;		//贴图高
+		STexInfo():tex(0), type(eColorMax), width(0), height(0){}
+	};
+
+	typedef vector<STexInfo*> TEX2DINFOLIST;//贴图信息列表
 public:
 	int sides;
 	int overlay_sides;
 	CDzGlWindow(int x, int y, int w, int h, const char *l = 0);
 	~CDzGlWindow(void);
 
+	void Release(void);
+
 	//加载材质
-	void LoadTexture(void);
+	//返回值:
+	//	成功:贴图句柄
+	//	失败:0
+	GLuint LoadPNGTexture(string szPath);
 private:
 	GLuint texName;
 	int m_nTextureHeight;
 	int m_nTextureWidth;
+
+	TEX2DINFOLIST m_TexList;//贴图信息列表
 };
